6b28065bbee7003ff01eeefda0ee56307467ee60
D3cl4n <dpmurphy@wpi.edu>
2024-09-02 15:26:39 -0400

    adding junk assembly instructions

--- HellsGate/hellsgate.asm
+++ HellsGate/hellsgate.asm
@@ -7,14 +7,17 @@
 		mov wSystemCall, 000h
 		nop
 		mov wSystemCall, ecx
+		xor ecx, ecx
 		nop
 		ret
 	FuncY ENDP
 
 	FuncX PROC
 		nop
+		xor r10, r10
 		mov r10, rcx
 		nop
+		xor eax, eax
 		mov eax, wSystemCall
 		nop
 		syscall
